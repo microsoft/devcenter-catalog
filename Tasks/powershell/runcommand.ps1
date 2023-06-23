2cc3e6dd9dac7b1b70cdcdad0aaf45eaf43a65a3
David Obando <daobando@microsoft.com>
2023-06-23 14:08:45 -0600

    Adding working directory to powershell task

--- Tasks/powershell/runcommand.ps1
+++ Tasks/powershell/runcommand.ps1
@@ -1,22 +1,35 @@
 param(
-     [Parameter()]
-     [string]$command
+    [Parameter()]
+    [string]$command,
+    [Parameter()]
+    [string]$workingDirectory
  )
 
-# We are adding this file as an additional layer
-# between the devbox.yaml file invoking this task
-# and the actual execution of the command.
-# The main goal is to ensure we do any additional
-# I/O here ahead of running the command on the metal.
+# Check if workingDirectory is set and not empty and if so, change to it.
+if ($workingDirectory -and $workingDirectory -ne "") {
+    # Check if the working directory exists.
+    if (-not (Test-Path $workingDirectory)) {
+        # Create the working directory if it does not exist.
+        Write-Output "Creating working directory $workingDirectory"
+        New-Item -ItemType Directory -Force -Path $workingDirectory
+    }
+
+    Write-Output "Changing to working directory $workingDirectory"
+    Set-Location $workingDirectory
+}
+
 # Note we're calling powershell.exe directly, instead
 # of running Invoke-Expression, as suggested by
 # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3
-# Also, note that this will run powershell.exe
+# Note that this will run powershell.exe
 # even if the system has pwsh.exe.
+Write-Output "Running command $command"
 powershell.exe -Command $command
 $commandExitCode = $LASTEXITCODE
 Write-Output "Command exited with code $commandExitCode"
 
 # Task powershell scripts should always end with an
 # exit code reported up to the runner agent.
+# This is how the runner agent knows whether the
+# command succeeded or failed.
 exit $commandExitCode
