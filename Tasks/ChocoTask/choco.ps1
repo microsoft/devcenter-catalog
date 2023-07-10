2aefa3b0e467fd2579a7195b30fdfc37d45a69c4
Antonella Avogadro <122132762+antonellaavogadro@users.noreply.github.com>
2023-07-10 11:51:55 -0700

    Removing strays

--- Tasks/ChocoTask/choco.ps1
+++ Tasks/ChocoTask/choco.ps1
@@ -35,7 +35,10 @@ function Ensure-Chocolatey
 
     if (-not (Test-Path "$ChocoExePath"))
     {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        $installScript = (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+        $expression = "$installScript | powershell.exe -NoProfile -ExecutionPolicy Bypass -Command -"
+        Execute -Expression $expression
         if ($LastExitCode -eq 3010)
         {
             Write-Host 'The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.'
@@ -68,13 +71,8 @@ function Install-Packages
         if ($PackageVersions.ContainsKey($name)) { # Check if the version was defined
             $version = "--version " + $PackageVersions[$name]
         }
-<<<<<<< HEAD
         $expression = "$ChocoExePath install -y -f --acceptlicense --no-progress --stoponfirstfailure $AdditionalOptions $name $version" # Change the command to install packages and versions
         Execute -Expression $expression
-=======
-        $expression = "$ChocoExePath install -y -f --acceptlicense --ignore-checksums --no-progress --stoponfirstfailure $AdditionalOptions $name $version" # Change the command to install packages and versions
-        Invoke-ExpressionImpl -Expression $expression
->>>>>>> 5875b8bbaa091390b11f09d8645e250421161671
     }
 }
 
@@ -87,17 +85,13 @@ function Execute
         $Expression
     )
 
-    # This call will normally not throw. So, when setting -ErrorVariable it causes it to throw.
-    # The variable $expError contains whatever is sent to stderr.
-
     # Note we're calling powershell.exe directly, instead
     # of running Invoke-Expression, as suggested by
     # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3
     # Note that this will run powershell.exe
     # even if the system has pwsh.exe.
     $process = Start-Process powershell.exe -ArgumentList "-Command $Expression" -NoNewWindow -PassThru -Wait
-    $expError = $process.StandardError.ReadToEnd()
-
+    $expError = $process.ExitCode.Exception
     # This check allows us to capture cases where the command we execute exits with an error code.
     # In that case, we do want to throw an exception with whatever is in stderr. Normally, when
     # Invoke-Expression throws, the error will come the normal way (i.e. $Error) and pass via the
@@ -130,10 +124,5 @@ Ensure-Chocolatey -ChocoExePath "$choco"
 
 Write-Host "Preparing to install Chocolatey packages: $Packages."
 Install-Packages -ChocoExePath "$choco" -Packages $Packages -PackageVersions $PackageVersions -AdditionalOptions $AdditionalOptions
-<<<<<<< HEAD
-
-#Start-Process powershell.exe -ArgumentList "-Command $expression" -NoNewWindow -Wait
-=======
->>>>>>> 5875b8bbaa091390b11f09d8645e250421161671
 
 Write-Host "`nThe artifact was applied successfully.`n"
