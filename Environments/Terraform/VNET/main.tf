8814eddb7ee7edd3bfbd43c1719abb9b948cbe15
Jack Kays <jackkays@microsoft.com>
2023-10-25 13:08:16 -0700

    adding baseline for Environments folder

---
+++ Environments/Terraform/VNET/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_virtual_network" "example" {
+  name                = format("%s-%s", var.name, "vnet")
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = format("%s-%s", var.name, "nsg")
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
