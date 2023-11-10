c101849e49e3e5e10403b134b19363952004aa3a
David Obando <daobando@microsoft.com>
2023-11-10 11:09:13 -0800

    Add ps-user task

---
+++ Experimental/ps-user/runAsUser.ps1
@@ -0,0 +1,15 @@
+$CustomizationScriptsDir = "C:\DevBoxCustomizations"
+$LockFile = "lockfile"
+$SetVariablesScript = "setVariables.ps1"
+$RunAsUserScript = "runAsUser.ps1"
+$CleanupScript = "cleanup.ps1"
+$RunAsUserTask = "DevBoxCustomizations"
+$CleanupTask = "DevBoxCustomizationsCleanup"
+
+Write-Host "Microsoft Dev Box - Customizations"
+Write-Host "----------------------------------"
+Write-Host "Setting up scheduled tasks..."
+
+Write-Host "Waiting on OneDrive initialization..."
+Start-Sleep -Seconds 120
+Remove-Item -Path "$($CustomizationScriptsDir)\$($LockFile)"
