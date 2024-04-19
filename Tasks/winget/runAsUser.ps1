38ec4b621a4a0fc450a88846d76c5ba339b73b2c
David Obando <daobando@microsoft.com>
2024-04-19 14:37:38 -0700

    Always run Repair-WinGetPackageManager in main.ps1

--- Tasks/winget/runAsUser.ps1
+++ Tasks/winget/runAsUser.ps1
@@ -26,28 +26,3 @@ catch {
     Write-Error "Failed to update WinGet"
     Write-Error $_
 }
-
-
-# install Microsoft.DesktopAppInstaller
-if (!(Get-AppxPackage Microsoft.DesktopAppInstaller -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Microsoft.DesktopAppInstaller"
-    # download the DesktopAppInstaller appx package to $env:TEMP
-    $tempFileName = [System.IO.Path]::GetRandomFileName()
-    $DesktopAppInstallerAppx = "$env:TEMP\$tempFileName-DesktopAppInstaller.appx"
-    try {
-        Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile $DesktopAppInstallerAppx
-
-        # install the DesktopAppInstaller appx package
-        Add-AppxPackage -Path $DesktopAppInstallerAppx -ForceApplicationShutdown
-
-        Write-Host "Done Installing Microsoft.DesktopAppInstaller"
-    }
-    catch {
-        Write-Error "Failed to install DesktopAppInstaller appx package"
-        Write-Error $_
-    }
-}
-else {
-    Write-Host "Microsoft.DesktopAppInstaller is already installed"
-}
-
