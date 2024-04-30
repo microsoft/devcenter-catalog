120233e458f748a53d2a8ba3ceec9ad4094dc6ca
David Obando <daobando@microsoft.com>
2024-04-30 10:19:42 -0700

    RunAsUser also checks for need for package updates

--- Tasks/winget/runAsUser.ps1
+++ Tasks/winget/runAsUser.ps1
@@ -18,38 +18,44 @@ Remove-Item -Path "$($CustomizationScriptsDir)\$($LockFile)"
 
 Write-Host "Updating WinGet"
 
-# instal Microsoft.UI.Xaml
-try{
-    $architecture = "x64"
-    if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
-        $architecture = "arm64"
+if (!(Get-AppxPackage -Name "Microsoft.UI.Xaml.2.8")){
+    # instal Microsoft.UI.Xaml
+    try{
+        $architecture = "x64"
+        if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
+            $architecture = "arm64"
+        }
+        $MsUiXaml = "$env:TEMP\$([System.IO.Path]::GetRandomFileName())-Microsoft.UI.Xaml.2.8.6"
+        $MsUiXamlZip = "$($MsUiXaml).zip"
+        Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.6" -OutFile $MsUiXamlZip
+        Expand-Archive $MsUiXamlZip -DestinationPath $MsUiXaml
+        Add-AppxPackage -Path "$($MsUiXaml)\tools\AppX\$($architecture)\Release\Microsoft.UI.Xaml.2.8.appx" -ForceApplicationShutdown
+        Write-Host "Done Installing Microsoft.UI.Xaml"
+    } catch {
+        Write-Error "Failed to install Microsoft.UI.Xaml"
+        Write-Error $_
     }
-    $MsUiXaml = "$env:TEMP\$([System.IO.Path]::GetRandomFileName())-Microsoft.UI.Xaml.2.8.6"
-    $MsUiXamlZip = "$($MsUiXaml).zip"
-    Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.6" -OutFile $MsUiXamlZip
-    Expand-Archive $MsUiXamlZip -DestinationPath $MsUiXaml
-    Add-AppxPackage -Path "$($MsUiXaml)\tools\AppX\$($architecture)\Release\Microsoft.UI.Xaml.2.8.appx" -ForceApplicationShutdown
-    Write-Host "Done Installing Microsoft.UI.Xaml"
-} catch {
-    Write-Error "Failed to install Microsoft.UI.Xaml"
-    Write-Error $_
 }
 
-# install Microsoft.DesktopAppInstaller
-try {
-    $DesktopAppInstallerAppx = "$env:TEMP\$([System.IO.Path]::GetRandomFileName())-DesktopAppInstaller.appx"
-    Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile $DesktopAppInstallerAppx
+$desktopAppInstallerPackage = Get-AppxPackage -Name "Microsoft.DesktopAppInstaller"
+if (!($desktopAppInstallerPackage) -or ($desktopAppInstallerPackage.Version -lt "1.22.0.0")) {
+    # install Microsoft.DesktopAppInstaller
+    try {
+        $DesktopAppInstallerAppx = "$env:TEMP\$([System.IO.Path]::GetRandomFileName())-DesktopAppInstaller.appx"
+        Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile $DesktopAppInstallerAppx
 
-    # install the DesktopAppInstaller appx package
-    Add-AppxPackage -Path $DesktopAppInstallerAppx -ForceApplicationShutdown
+        # install the DesktopAppInstaller appx package
+        Add-AppxPackage -Path $DesktopAppInstallerAppx -ForceApplicationShutdown
 
-    Write-Host "Done Installing Microsoft.DesktopAppInstaller"
-}
-catch {
-    Write-Error "Failed to install DesktopAppInstaller appx package"
-    Write-Error $_
+        Write-Host "Done Installing Microsoft.DesktopAppInstaller"
+    }
+    catch {
+        Write-Error "Failed to install DesktopAppInstaller appx package"
+        Write-Error $_
+    }
 }
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Host "Done Updating WinGet"
 
 
