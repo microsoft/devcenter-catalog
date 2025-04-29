7c1dc42c77c0d5a899132390cc7aa10b7d3cd1d7
Max Horstmann <maxhorstmann@microsoft.com>
2025-04-29 10:08:13 -0400

    add -Mode Silent to Winget task

--- Tasks/winget/main.ps1
+++ Tasks/winget/main.ps1
@@ -373,7 +373,7 @@ else {
         }
 
         $installCommandBlock = {
-            $installPackageCommand = "Install-WinGetPackage -Scope $($scopeFlagValue) -Source winget -Id '$($Package)' $($versionFlag) | ConvertTo-Json -Depth 10 | Tee-Object -FilePath '$($tempOutFile)'"
+            $installPackageCommand = "Install-WinGetPackage -Scope $($scopeFlagValue) -Mode Silent -Source winget -Id '$($Package)' $($versionFlag) | ConvertTo-Json -Depth 10 | Tee-Object -FilePath '$($tempOutFile)'"
             $processCreation = Invoke-CimMethod -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine="C:\Program Files\PowerShell\7\pwsh.exe $($mtaFlag) -Command `"$($installPackageCommand)`""}
             if (!($processCreation) -or !($processCreation.ProcessId)) {
                 Write-Error "Failed to install package. Process creation failed."
